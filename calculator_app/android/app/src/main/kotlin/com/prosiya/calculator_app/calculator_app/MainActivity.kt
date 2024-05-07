676203718b28176f51259a637de0b68a8f3ab5ee
prosiyashrestha <prosiyashrestha@gmail.com>
2024-05-07 11:20:48 +0545

    Calculator

---
+++ calculator_app/android/app/src/main/kotlin/com/prosiya/calculator_app/calculator_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.prosiya.calculator_app.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calculator_app/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
+  
